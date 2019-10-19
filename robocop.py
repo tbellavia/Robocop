07bd4af50ddd6e7da70a0a410c93158c5f17074d
Tony Bellavia <at.bellavia@gmail.com>
2019-10-19 23:23:36 +0200

    finished fixing channel problem

--- robocop.py
+++ robocop.py
@@ -3,13 +3,13 @@ import discord
 import re
 from dotenv import load_dotenv
 from robocop.err_messages import ErrorMessages
+from urlextract import URLExtract
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
-
 
 class Robocop(discord.Client):
     async def on_ready(self):
@@ -24,31 +24,33 @@ class Robocop(discord.Client):
             "print", "len", "[", "]", "{", "}"
         ]
         self.code_channels = [
-            "aide-python", "aide-autres-langages", 
-            "aide-web", "aide-vfx", 
-            "différences-entre-versions", "tutorat",
-            "aide-datascience", "projets-datascience",
-            "machine-learning", "deep-learning",
-            "la-formation-complète-python", "interface-graphiques-avec-pyside"
+            633826042741653510
+        ]
+        self.forbidden_channels = [
+            635213581926924311
         ]
         print(f"{client.user} has connected to Discord!")
 
-    async def on_message(self, message):
+    def is_not_markdown(self, message):
         regex = re.compile(r"(`{3}(.|\n)*`{3})")
+        if regex.match(message) is None:
+            return True
+        return False
 
-        if regex.match(message.content) is None:
+    async def on_message(self, message):
+        if self.is_not_markdown(message.content):
             splitted_message = message.content.split()
             counter = sum([splitted_message.count(keyword) for keyword in self.reserved_keywords])
             counter += sum([1 for keyword in self.reserved_keywords if keyword in splitted_message])
 
             if counter >= 5:
-                if message.channel in self.code_channels:
+                if message.channel.id in self.code_channels:
                     await message.channel.send(
-                        f"{ErrorMessages.INAPPROPRIATE_CHANNEL_CODE.value.replace('<PLACEHOLDER>', message.author.mention)}"
+                        f"{ErrorMessages.MARKDOWN_FAIL.value.replace('<PLACEHOLDER>', message.author.mention)}"
                     )
                 else:
                     await message.channel.send(
-                        f"{ErrorMessages.MARKDOWN_FAIL.value.replace('<PLACEHOLDER>', message.author.mention)}"
+                        f"{ErrorMessages.INAPPROPRIATE_CHANNEL_CODE.value.replace('<PLACEHOLDER>', message.author.mention)}"
                     )
 
 client = Robocop()
