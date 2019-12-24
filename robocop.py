11792becf50e7b7a135430d590fcf8d1bdc1156e
Tony Bellavia <at.bellavia@gmail.com>
2019-12-24 14:05:49 +0100

    adding xmas features

--- robocop.py
+++ robocop.py
@@ -88,7 +88,9 @@ class Robocop(discord.Client):
         # Salute
         for mention in message.mentions:
             if mention.id == self.bot_id:
-                salutes = Expressions.SALUTE.value
+                # salutes = Expressions.SALUTE.value
+                # Commenting for XMAS celebrationm replacing messages by xmas expressions
+                salutes = Expressions.XMAS_SALUTE.value
                 await message.channel.send(
                     f"{salutes[randint(0, len(salutes) - 1)]} {message.author.mention}"
                 )
--- robocop/expressions.py
+++ robocop/expressions.py
@@ -7,3 +7,8 @@ class Expressions(Enum):
         "Bip bop",
         "https://media.giphy.com/media/iIRRfusU6ldV4kyPdd/giphy.gif"
     ]
+    XMAS_SALUTE = [
+        "Joyeux noël mon ami.e !",
+        "Joyeux réveillon, pythonista !",
+        "Joyeux noël et puisse le code être avec toi !"
+    ]
